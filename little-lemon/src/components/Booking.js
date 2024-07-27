5d0c8d51a45d5afdd3eea065c9f004906e5520e7
SpurtiR <104018704+SpurtiR@users.noreply.github.com>
2024-07-27 20:40:26 +0530

    Delete little-lemon directory

--- little-lemon/src/components/Booking.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import BookingForm from './BookingForm'
- 
-export default function Booking(props) {
-  return (
-    <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch} SubmitForm={props.SubmitForm}/>
-  )
-}
--- little-lemon/src/components/ConfirmedBooking.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ConfirmedBooking() {
-  return (
-    <div className='confirm'>
-        <h1>Booking has been <span>confirmed!</span></h1>
-    </div>
-  )
-}
